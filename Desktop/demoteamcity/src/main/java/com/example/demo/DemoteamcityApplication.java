e7335c03cf65605316deff530c6a2056ddab240f
nguyenthinh123 <nguyenthinh.nau@gmail.com>
2021-06-18 09:33:29 +0700

    Initial commit

---
+++ Desktop/demoteamcity/src/main/java/com/example/demo/DemoteamcityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoteamcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoteamcityApplication.class, args);
+	}
+
+}
